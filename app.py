6c221864f0946aaf8ea69cf659d23705f33f6a5e
aabdurrohim <abdurrohim.i.p@gmail.com>
2025-02-03 12:39:07 +0700

    updarte

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from db import dbProject  # Assuming this is where your project data is
 from dotenv import load_dotenv
 import os
 
